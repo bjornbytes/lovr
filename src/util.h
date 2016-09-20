55481a1e535f3eb03f74e38c4542691d26bef3ef
bjorn <tie.372@gmail.com>
2016-09-19 23:43:03 -0700

    rm matrixMultiply;

--- src/util.h
+++ src/util.h
@@ -8,4 +8,3 @@ char* loadFile(char* filename);
 void luaRegisterModule(lua_State* L, const char* name, const luaL_Reg* module);
 void luaRegisterType(lua_State* L, const char* name, const luaL_Reg* functions, lua_CFunction gc);
 int luaPreloadModule(lua_State* L, const char* key, lua_CFunction f);
-float* matrixMultiply(float* a, float* b);
