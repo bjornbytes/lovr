53409920c6a1a275e65683ac3a9f8d043af6d4b8
bjorn <tie.372@gmail.com>
2016-10-16 21:21:26 -0700

    Skybox texture parameters;

--- src/graphics/skybox.c
+++ src/graphics/skybox.c
@@ -1,4 +1,5 @@
 #include "skybox.h"
+#include "../util.h"
 #include <stdlib.h>
 
 Skybox* lovrSkyboxCreate(char** filenames) {
@@ -10,8 +11,19 @@ Skybox* lovrSkyboxCreate(char** filenames) {
   for (int i = 0; i < 6; i++) {
     int width, height, channels;
     unsigned char* data = stbi_load(filenames[i], &width, &height, &channels, 3);
+
+    if (data == NULL) {
+      error("Could not load image %s", filenames[i]);
+    }
+
     glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
   }
+
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
   return skybox;
 }
