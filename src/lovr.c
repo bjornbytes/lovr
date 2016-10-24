b3cd1a103e85fc1f713472cc54be5f1e556b721a
bjorn <tie.372@gmail.com>
2016-10-24 13:39:09 -0700

    Improve error messages in main.lua;

--- src/lovr.c
+++ src/lovr.c
@@ -85,8 +85,7 @@ void lovrRun(lua_State* L, char* root) {
 
   // Run "main.lua" which will override/define pieces of lovr
   if (fileExists(path) && luaL_dofile(L, path)) {
-    error("Failed to run main.lua");
-    lua_pop(L, 1);
+    lovrOnLuaError(L);
     exit(EXIT_FAILURE);
   }
 
