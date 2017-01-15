66fbcceda8a036a25b581990cf50a0b2b305ae00
bjorn <tie.372@gmail.com>
2017-01-15 14:13:33 -0800

    v0.3.0;

--- src/lovr.h
+++ src/lovr.h
@@ -3,7 +3,7 @@
 #include <lualib.h>
 
 #define LOVR_VERSION_MAJOR 0
-#define LOVR_VERSION_MINOR 2
+#define LOVR_VERSION_MINOR 3
 #define LOVR_VERSION_PATCH 0
 
 void lovrInit(lua_State* L, int argc, char** argv);
