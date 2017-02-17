063f1d44006bf4649374625fadf2fce066f51619
bjorn <tie.372@gmail.com>
2017-02-17 00:49:13 -0800

    0.4.0;

--- src/lovr.h
+++ src/lovr.h
@@ -1,7 +1,7 @@
 #include "luax.h"
 
 #define LOVR_VERSION_MAJOR 0
-#define LOVR_VERSION_MINOR 3
+#define LOVR_VERSION_MINOR 4
 #define LOVR_VERSION_PATCH 0
 
 void lovrInit(lua_State* L, int argc, char** argv);
