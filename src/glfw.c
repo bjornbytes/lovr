49a63bb0845ab522ac302f4c9dfd8f86603f6438
bjorn <tie.372@gmail.com>
2017-03-31 03:57:04 -0700

    Antialiasing;
    Why not.

--- src/glfw.c
+++ src/glfw.c
@@ -18,6 +18,7 @@ void initGlfw() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_SAMPLES, 4);
 
   window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
 
