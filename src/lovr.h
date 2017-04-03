30647c97c5f5c056baaa3d6eeb78a3e53cc1722e
bjorn <tie.372@gmail.com>
2017-04-03 09:40:37 -0700

    0.5.0;

--- src/lovr.h
+++ src/lovr.h
@@ -1,9 +1,9 @@
 #include "luax.h"
 
 #define LOVR_VERSION_MAJOR 0
-#define LOVR_VERSION_MINOR 4
+#define LOVR_VERSION_MINOR 5
 #define LOVR_VERSION_PATCH 0
-#define LOVR_VERSION_ALIAS "Cosmic Panda"
+#define LOVR_VERSION_ALIAS "Psychotic Octopus"
 
 void lovrInit(lua_State* L, int argc, char** argv);
 void lovrDestroy(int exitCode);
