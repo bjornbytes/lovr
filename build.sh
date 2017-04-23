9cc33fc5a269cf5eadcc3ea88ce7a5a31e6ce2cd
bjorn <tie.372@gmail.com>
2017-04-22 20:03:54 -0700

    Remove scripts;

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf build
-mkdir build
-cd build
-emcmake cmake .. && emmake make
-mkdir game
-echo "function lovr.load()\n  print('hi')\nend" > game/main.lua
-../package.sh game
--- package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python "$EMSCRIPTEN/tools/file_packager.py" game.data --preload "$1"@/ --js-output=game.js
