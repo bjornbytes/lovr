10ddc06c13e1c0fa28580ea7a0bbca09feb3275e
bjorn <tie.372@gmail.com>
2017-06-01 23:52:37 -0700

    No resizable window;

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -32,6 +32,7 @@ void lovrGraphicsInit() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 #endif
 
   state.window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
