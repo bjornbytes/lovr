e705e898b2c3605c20dd56f5ba298026eadd0eac
bjorn <tie.372@gmail.com>
2017-06-23 19:43:14 -0700

    0.7.1;

--- src/lovr.h
+++ src/lovr.h
@@ -2,7 +2,7 @@
 
 #define LOVR_VERSION_MAJOR 0
 #define LOVR_VERSION_MINOR 7
-#define LOVR_VERSION_PATCH 0
+#define LOVR_VERSION_PATCH 1
 #define LOVR_VERSION_ALIAS "Pesto Junkie"
 
 void lovrInit(lua_State* L, int argc, char** argv);
