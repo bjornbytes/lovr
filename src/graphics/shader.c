c750b64b1bca2d5dce4db7509c1c607b0ab792ed
bjorn <tie.372@gmail.com>
2017-07-15 02:08:40 +0900

    Cleanup;

--- src/graphics/shader.c
+++ src/graphics/shader.c
@@ -16,8 +16,7 @@ const char* lovrShaderVertexPrefix = ""
 "out vec2 texCoord; \n"
 "uniform mat4 lovrTransform; \n"
 "uniform mat3 lovrNormalMatrix; \n"
-"uniform mat4 lovrProjection; \n"
-"";
+"uniform mat4 lovrProjection; \n";
 
 const char* lovrShaderFragmentPrefix = ""
 #ifdef LOVR_WEB
@@ -30,49 +29,42 @@ const char* lovrShaderFragmentPrefix = ""
 "in vec2 texCoord; \n"
 "out vec4 lovrFragColor; \n"
 "uniform vec4 lovrColor; \n"
-"uniform sampler2D lovrTexture; \n"
-"";
+"uniform sampler2D lovrTexture; \n";
 
 const char* lovrShaderVertexSuffix = ""
 "void main() { \n"
 "  texCoord = lovrTexCoord; \n"
 "  gl_Position = position(lovrProjection, lovrTransform, vec4(lovrPosition, 1.0)); \n"
-"}"
-"";
+"}";
 
 const char* lovrShaderFragmentSuffix = ""
 "void main() { \n"
 "  lovrFragColor = color(lovrColor, lovrTexture, texCoord); \n"
-"}"
-"";
+"}";
 
 const char* lovrDefaultVertexShader = ""
 "vec4 position(mat4 projection, mat4 transform, vec4 vertex) { \n"
 "  return projection * transform * vertex; \n"
-"}"
-"";
+"}";
 
 const char* lovrDefaultFragmentShader = ""
 "vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) { \n"
 "  return graphicsColor * texture(image, uv); \n"
-"}"
-"";
+"}";
 
 const char* lovrSkyboxVertexShader = ""
 "out vec3 texturePosition; \n"
 "vec4 position(mat4 projection, mat4 transform, vec4 vertex) { \n"
 "  texturePosition = vertex.xyz; \n"
 "  return projection * transform * vertex; \n"
-"}"
-"";
+"}";
 
 const char* lovrSkyboxFragmentShader = ""
 "in vec3 texturePosition; \n"
 "uniform samplerCube cube; \n"
 "vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) { \n"
 "  return graphicsColor * texture(cube, texturePosition); \n"
-"}"
-"";
+"}";
 
 const char* lovrFontFragmentShader = ""
 "float median(float r, float g, float b) { \n"
@@ -84,14 +76,12 @@ const char* lovrFontFragmentShader = ""
 "  float w = fwidth(sdf); \n"
 "  float alpha = smoothstep(.5 - w, .5 + w, sdf); \n"
 "  return vec4(graphicsColor.rgb, alpha); \n"
-"}"
-"";
+"}";
 
 const char* lovrNoopVertexShader = ""
 "vec4 position(mat4 projection, mat4 transform, vec4 vertex) { \n"
 "  return vertex; \n"
-"}"
-"";
+"}";
 
 GLuint compileShader(GLenum type, const char* source) {
   GLuint shader = glCreateShader(type);
