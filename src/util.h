f91366aa96a3649e0437b9c005a8cbd37165f381
bjorn <tie.372@gmail.com>
2017-08-08 22:31:27 -0700

    Remove old color macros;

--- src/util.h
+++ src/util.h
@@ -5,11 +5,6 @@
 #pragma once
 
 #define containerof(ptr, type) ((type*)((char*)(ptr) - offsetof(type, ref)))
-#define LOVR_COLOR(r, g, b, a) ((a << 0) | (b << 8) | (g << 16) | (r << 24))
-#define LOVR_COLOR_R(c) (c >> 24 & 0xff)
-#define LOVR_COLOR_G(c) (c >> 16 & 0xff)
-#define LOVR_COLOR_B(c) (c >> 8  & 0xff)
-#define LOVR_COLOR_A(c) (c >> 0  & 0xff)
 
 typedef vec_t(unsigned int) vec_uint_t;
 
