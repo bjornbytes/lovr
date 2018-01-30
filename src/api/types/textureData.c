e859350e253e709a474f6104afcb5c11bf18b560
bjorn <tie.372@gmail.com>
2018-01-29 19:15:58 -0800

    TextureData:getDimensions;

--- src/api/types/textureData.c
+++ src/api/types/textureData.c
@@ -12,8 +12,16 @@ int l_lovrTextureDataGetHeight(lua_State* L) {
   return 1;
 }
 
+int l_lovrTextureDataGetDimensions(lua_State* L) {
+  TextureData* textureData = luax_checktype(L, 1, TextureData);
+  lua_pushinteger(L, textureData->width);
+  lua_pushinteger(L, textureData->height);
+  return 2;
+}
+
 const luaL_Reg lovrTextureData[] = {
   { "getWidth", l_lovrTextureDataGetWidth },
   { "getHeight", l_lovrTextureDataGetHeight },
+  { "getDimensions", l_lovrTextureDataGetDimensions },
   { NULL, NULL }
 };
