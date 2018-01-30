78774d65be7a84e713a57ab4f75d1a3fe56dfb92
bjorn <tie.372@gmail.com>
2018-01-29 19:09:43 -0800

    Actually hold off on node APIs for now;

--- src/api/types/modelData.c
+++ src/api/types/modelData.c
@@ -1,30 +1,21 @@
 #include "api.h"
 #include "data/model.h"
 
-static int nextChild(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, lua_upvalueindex(1), ModelData);
-  ModelNode* node = lua_touserdata(L, lua_upvalueindex(2));
-  int index = lua_tonumber(L, lua_upvalueindex(3));
-  if (index >= node->children.length) {
-    lua_pushnil(L);
-  } else {
-    lua_pushlightuserdata(L, &modelData->nodes[node->children.data[index++]]);
-    lua_pushnumber(L, index);
-    lua_replace(L, lua_upvalueindex(3));
-  }
+int l_lovrModelDataGetNodeCount(lua_State* L) {
+  ModelData* modelData = luax_checktype(L, 1, ModelData);
+  lua_pushinteger(L, modelData->nodeCount);
   return 1;
 }
 
-static int nextNode(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, lua_upvalueindex(1), ModelData);
-  int index = lua_tonumber(L, lua_upvalueindex(2));
-  if (index >= modelData->nodeCount) {
-    lua_pushnil(L);
-  } else {
-    lua_pushlightuserdata(L, &modelData->nodes[index++]);
-    lua_pushnumber(L, index);
-    lua_replace(L, lua_upvalueindex(2));
-  }
+int l_lovrModelDataGetAnimationCount(lua_State* L) {
+  ModelData* modelData = luax_checktype(L, 1, ModelData);
+  lua_pushinteger(L, modelData->animationCount);
+  return 1;
+}
+
+int l_lovrModelDataGetMaterialCount(lua_State* L) {
+  ModelData* modelData = luax_checktype(L, 1, ModelData);
+  lua_pushinteger(L, modelData->materialCount);
   return 1;
 }
 
@@ -47,89 +38,18 @@ int l_lovrModelDataGetVertex(lua_State* L) {
   return luax_pushvertex(L, &vertex, &modelData->format);
 }
 
-int l_lovrModelDataGetFaceCount(lua_State* L) {
+int l_lovrModelDataGetTriangleCount(lua_State* L) {
   ModelData* modelData = luax_checktype(L, 1, ModelData);
   lua_pushinteger(L, modelData->indexCount / 3);
   return 1;
 }
 
-int l_lovrModelDataGetFace(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, 1, ModelData);
-  int index = luaL_checkint(L, 2) - 1;
-  if (modelData->indexSize == sizeof(uint32_t)) {
-    lua_pushinteger(L, modelData->indices.ints[index * 3 + 0]);
-    lua_pushinteger(L, modelData->indices.ints[index * 3 + 1]);
-    lua_pushinteger(L, modelData->indices.ints[index * 3 + 2]);
-  } else {
-    lua_pushinteger(L, modelData->indices.shorts[index * 3 + 0]);
-    lua_pushinteger(L, modelData->indices.shorts[index * 3 + 1]);
-    lua_pushinteger(L, modelData->indices.shorts[index * 3 + 2]);
-  }
-  return 3;
-}
-
-int l_lovrModelDataGetNodeCount(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, 1, ModelData);
-  lua_pushinteger(L, modelData->nodeCount);
-  return 1;
-}
-
-int l_lovrModelDataGetRoot(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, 1, ModelData);
-  if (modelData->nodeCount == 0) {
-    lua_pushnil(L);
-  } else {
-    ModelNode* root = &modelData->nodes[0];
-    lua_pushlightuserdata(L, root);
-  }
-  return 1;
-}
-
-int l_lovrModelDataGetNodeName(lua_State* L) {
-  ModelNode* node = lua_touserdata(L, 2);
-  lua_pushstring(L, node->name);
-  return 1;
-}
-
-int l_lovrModelDataGetNodeParent(lua_State* L) {
-  ModelData* modelData = luax_checktype(L, 1, ModelData);
-  ModelNode* node = lua_touserdata(L, 2);
-  if (node->parent == -1) {
-    lua_pushnil(L);
-  } else {
-    lua_pushlightuserdata(L, &modelData->nodes[node->parent]);
-  }
-  return 1;
-}
-
-int l_lovrModelDataChildren(lua_State* L) {
-  luax_checktype(L, 1, ModelData);
-  luaL_checktype(L, 2, LUA_TLIGHTUSERDATA);
-  lua_settop(L, 2);
-  lua_pushinteger(L, 0);
-  lua_pushcclosure(L, nextChild, 3);
-  return 1;
-}
-
-int l_lovrModelDataNodes(lua_State* L) {
-  luax_checktype(L, 1, ModelData);
-  lua_settop(L, 1);
-  lua_pushinteger(L, 0);
-  lua_pushcclosure(L, nextNode, 2);
-  return 1;
-}
-
 const luaL_Reg lovrModelData[] = {
+  { "getNodeCount", l_lovrModelDataGetNodeCount },
+  { "getAnimationCount", l_lovrModelDataGetAnimationCount },
+  { "getMaterialCount", l_lovrModelDataGetMaterialCount },
   { "getVertexFormat", l_lovrModelDataGetVertexFormat },
   { "getVertexCount", l_lovrModelDataGetVertexCount },
-  { "getVertex", l_lovrModelDataGetVertex },
-  { "getFaceCount", l_lovrModelDataGetFaceCount },
-  { "getFace", l_lovrModelDataGetFace },
-  { "getNodeCount", l_lovrModelDataGetNodeCount },
-  { "getRoot", l_lovrModelDataGetRoot },
-  { "getNodeName", l_lovrModelDataGetNodeName },
-  { "getNodeParent", l_lovrModelDataGetNodeParent },
-  { "children", l_lovrModelDataChildren },
-  { "nodes", l_lovrModelDataNodes },
+  { "getTriangleCount", l_lovrModelDataGetTriangleCount },
   { NULL, NULL }
 };
