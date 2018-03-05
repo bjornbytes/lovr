2cbd565522be8c9796eb12ff2ae43fd926d79130
bjorn <tie.372@gmail.com>
2018-03-05 02:42:38 -0800

    Fix texture submit bounds; Fix layer canvas bug;

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -373,7 +373,7 @@ void lovrGraphicsSetCanvas(Canvas** canvas, int count) {
   if (count == 0) {
     Layer layer = state.layers[state.layer];
     int* viewport = layer.viewport;
-    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->texture.id : 0);
+    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->framebuffer : 0);
     lovrGraphicsSetViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
   } else {
     memcpy(state.canvas, canvas, count * sizeof(Canvas*));
@@ -1102,7 +1102,7 @@ void lovrGraphicsPushLayer(Layer layer) {
 
   if (state.canvasCount == 0) {
     int* viewport = layer.viewport;
-    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->texture.id : 0);
+    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->framebuffer : 0);
     lovrGraphicsSetViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
   }
 }
@@ -1115,7 +1115,7 @@ void lovrGraphicsPopLayer() {
   if (state.canvasCount == 0) {
     Layer layer = state.layers[state.layer];
     int* viewport = layer.viewport;
-    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->texture.id : 0);
+    lovrGraphicsBindFramebuffer(layer.canvas ? layer.canvas->framebuffer : 0);
     lovrGraphicsSetViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
   }
 }
