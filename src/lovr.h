8a7054e793ceea85c6fff8d5e2fcce6378786d4f
bjorn <tie.372@gmail.com>
2018-09-05 08:02:50 -0700

    v0.11.0;

--- src/lovr.h
+++ src/lovr.h
@@ -1,9 +1,9 @@
 #include <stdbool.h>
 
 #define LOVR_VERSION_MAJOR 0
-#define LOVR_VERSION_MINOR 10
+#define LOVR_VERSION_MINOR 11
 #define LOVR_VERSION_PATCH 0
-#define LOVR_VERSION_ALIAS "Hangry Goose"
+#define LOVR_VERSION_ALIAS "Ginormous Giraffe"
 
 void lovrDestroy();
 bool lovrRun(int argc, char** argv, int* status);
