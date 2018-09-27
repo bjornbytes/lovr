3e2259fc99f08e1d99747eefa73f8cb8af5864d8
bjorn <tie.372@gmail.com>
2018-09-26 21:45:45 -0700

    version/os refactor;

--- src/lovr.h
+++ src/lovr.h
@@ -1,11 +1,4 @@
 #include <stdbool.h>
 
-#define LOVR_VERSION_MAJOR 0
-#define LOVR_VERSION_MINOR 11
-#define LOVR_VERSION_PATCH 0
-#define LOVR_VERSION_ALIAS "Ginormous Giraffe"
-
 bool lovrRun(int argc, char** argv, int* status);
 void lovrQuit(int status);
-const char* lovrGetOS();
-void lovrGetVersion(int* major, int* minor, int* patch);
---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define LOVR_VERSION_MAJOR 0
+#define LOVR_VERSION_MINOR 11
+#define LOVR_VERSION_PATCH 0
+#define LOVR_VERSION_ALIAS "Ginormous Giraffe"
