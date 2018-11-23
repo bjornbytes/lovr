8ddb340a28baaf332aa156869a4aa330fb1ecceb
mcc <andi.m.mcclure@gmail.com>
2018-11-22 20:09:33 -0500

    Android lua module
    Currently provides only a lovr.android.getApplicationId(). This returns an Android-specific identifier that doesn't cleanly map to anything specific in other OSes.

---
+++ src/api/android.c
@@ -0,0 +1,31 @@
+#include "api.h"
+#include "sds.h"
+
+static int l_lovrGetApplicationId(lua_State *L) {
+  pid_t pid = getpid();
+  sds procPath = sdscatfmt(sdsempty(), "/proc/%d/cmdline", (int)pid);
+  FILE *procFile = fopen(procPath, "r");
+  sdsfree(procPath);
+  if (procFile) {
+    sds procData = sdsempty();
+    char data[64];
+    int read;
+    while (read = fread(data, sizeof(data), 1, procFile)) {
+      procData = sdscatlen(procData, data, read);
+    }
+    sdsfree(procData);
+  } else {
+    lua_pushnil(L);
+  }
+  return 1;
+}
+
+static const luaL_Reg lovrAndroid[] = {
+  { "getApplicationId", l_lovrGetApplicationId },
+}
+
+LOVR_API int luaopen_lovr_android(lua_State* L) {
+  lua_newtable(L);
+  luaL_register(L, NULL, lovrAndroid);
+  return 1;
+}
