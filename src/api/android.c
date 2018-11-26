adabf4c6ef7fe0c56c328ea9a4e364edc98235a4
mcc <andi.m.mcclure@gmail.com>
2018-11-26 02:12:21 -0500

    Fix broken android lua module
    The LOVR_ENABLE_ANDROID flag wasn't getting passed to C, and the getApplicationId code had bugs

--- src/api/android.c
+++ src/api/android.c
@@ -1,19 +1,21 @@
 #include "api.h"
 #include "lib/sds/sds.h"
 #include <unistd.h>
+#include "platform.h"
 
 static int l_lovrGetApplicationId(lua_State *L) {
   pid_t pid = getpid();
-  sds procPath = sdscatfmt(sdsempty(), "/proc/%d/cmdline", (int)pid);
+  sds procPath = sdscatfmt(sdsempty(), "/proc/%i/cmdline", (int)pid);
   FILE *procFile = fopen(procPath, "r");
   sdsfree(procPath);
   if (procFile) {
     sds procData = sdsempty();
     char data[64];
     int read;
-    while ((read = fread(data, sizeof(data), 1, procFile))) {
+    while ((read = fread(data, 1, sizeof(data), procFile))) {
       procData = sdscatlen(procData, data, read);
     }
+    lua_pushstring(L, procData);
     sdsfree(procData);
   } else {
     lua_pushnil(L);
