e87b81e34a2ddcd878d1297fe0e40ce2d01f472c
bjorn <tie.372@gmail.com>
2019-05-20 15:16:27 -0700

    Fix android platform include order;

--- src/core/platform.c
+++ src/core/platform.c
@@ -4,10 +4,10 @@
 #include "platform_windows.c.h"
 #elif __APPLE__
 #include "platform_macos.c.h"
+#elif __ANDROID__
+#include "platform_android.c.h"
 #elif __linux__
 #include "platform_linux.c.h"
 #elif EMSCRIPTEN
 #include "platform_web.c.h"
-#elif __ANDROID__
-#include "platform_android.c.h"
 #endif
