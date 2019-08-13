d3bf51daefb0c9dbb5a754f8e8b5131d6cbff119
bjorn <tie.372@gmail.com>
2019-08-12 17:38:03 -0700

    Add alphaCutoff shader flag;

--- src/resources/shaders.c
+++ src/resources/shaders.c
@@ -97,6 +97,11 @@ const char* lovrShaderFragmentSuffix = ""
 "  colors(lovrColor, lovrDiffuseTexture, texCoord); \n"
 "#else \n"
 "  lovrCanvas[0] = color(lovrColor, lovrDiffuseTexture, texCoord); \n"
+"#ifdef FLAG_alphaCutoff \n"
+"  if (lovrCanvas[0].a < FLAG_alphaCutoff) { \n"
+"    discard; \n"
+"  } \n"
+"#endif \n"
 #ifdef LOVR_WEBGL
 "  lovrCanvas[0].rgb = pow(lovrCanvas[0].rgb, vec3(.4545)); \n"
 #endif
