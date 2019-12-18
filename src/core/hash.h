ff1ae2c050f70fb042d8a526bd0f54dc0a06bf20
mcc <andi.m.mcclure@gmail.com>
2019-12-17 20:02:33 -0500

    Minor fix to let header files compile in C++

--- src/core/hash.h
+++ src/core/hash.h
@@ -3,7 +3,7 @@
 
 // FNV1a
 static LOVR_INLINE uint64_t hash64(const void* data, size_t length) {
-  const uint8_t* bytes = data;
+  const uint8_t* bytes = (uint8_t *)data;
   uint64_t hash = 0xcbf29ce484222325;
   for (size_t i = 0; i < length; i++) {
     hash = (hash ^ bytes[i]) * 0x100000001b3;
