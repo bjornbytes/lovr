f6abb8d71407c2b801a2bd9b53e636602936aaa6
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-01-02 20:32:50 +0100

    Raw audiostream implementation
    Naive unoptimized implementation to get a feel
    for the API design.

--- src/modules/data/audioStream.c
+++ src/modules/data/audioStream.c
@@ -1,5 +1,6 @@
 #include "data/audioStream.h"
 #include "data/blob.h"
+#include "data/soundData.h"
 #include "core/ref.h"
 #include "core/util.h"
 #include "lib/stb/stb_vorbis.h"
@@ -24,11 +25,47 @@ AudioStream* lovrAudioStreamInit(AudioStream* stream, Blob* blob, size_t bufferS
   return stream;
 }
 
+AudioStream* lovrAudioStreamInitRaw(AudioStream* stream, size_t bufferSize, int channelCount, int sampleRate) {
+  stream->bitDepth = 16;
+  stream->channelCount = channelCount;
+  stream->sampleRate = sampleRate;
+  stream->samples = SIZE_MAX;
+  stream->decoder = NULL;
+  stream->bufferSize = stream->channelCount * bufferSize * sizeof(int16_t);
+  stream->buffer = malloc(stream->bufferSize);
+  lovrAssert(stream->buffer, "Out of memory");
+  stream->blob = NULL;
+  arr_init(&stream->queuedRawBuffers);
+  return stream;
+}
+
 void lovrAudioStreamDestroy(void* ref) {
   AudioStream* stream = ref;
   stb_vorbis_close(stream->decoder);
   lovrRelease(Blob, stream->blob);
   free(stream->buffer);
+}
+
+static size_t dequeue_raw(AudioStream* stream, int16_t* destination, size_t size)
+{
+  if (stream->queuedRawBuffers.length == 0) {
+    return 0;
+  }
+  Blob* blob = stream->queuedRawBuffers.data[0];
+  if (blob->size <= size) {
+    // blob fits in destination in its entirety. Copy over, free it and remove from start of array.
+    memcpy(destination, blob->data, blob->size);
+    lovrRelease(Blob, blob);
+    arr_splice(&stream->queuedRawBuffers, 0, 1);
+    return size;
+  } else {
+    // blob is too big. copy all that fits, and put remainder in a new blob in its place.
+    memcpy(destination, blob->data, size);
+    void* oldData = blob->data;
+    lovrBlobInit(blob, (char*)blob->data + size, blob->size - size, "raw audiostream remainder");
+    free(oldData);
+    return blob->size;
+  }
 }
 
 size_t lovrAudioStreamDecode(AudioStream* stream, int16_t* destination, size_t size) {
@@ -39,7 +76,12 @@ size_t lovrAudioStreamDecode(AudioStream* stream, int16_t* destination, size_t s
   size_t samples = 0;
 
   while (samples < capacity) {
-    int count = stb_vorbis_get_samples_short_interleaved(decoder, channelCount, buffer + samples, (int) (capacity - samples));
+    int count = 0;
+    if (decoder) {
+      count = stb_vorbis_get_samples_short_interleaved(decoder, channelCount, buffer + samples, (int)(capacity - samples));
+    } else {
+      count = dequeue_raw(stream, buffer + samples, (int)(capacity - samples));
+    }
     if (count == 0) break;
     samples += count * channelCount;
   }
@@ -47,17 +89,32 @@ size_t lovrAudioStreamDecode(AudioStream* stream, int16_t* destination, size_t s
   return samples;
 }
 
+void lovrAudioStreamAppendRawBlob(AudioStream* stream, struct Blob* blob)
+{
+  lovrRetain(blob);
+  arr_push(&stream->queuedRawBuffers, blob);
+}
+
+void lovrAudioStreamAppendRawSound(AudioStream* stream, struct SoundData* sound)
+{
+  lovrAssert(sound->channelCount == stream->channelCount && sound->bitDepth == stream->bitDepth && sound->sampleRate == stream->sampleRate, "SoundData and AudioStream formats must match");
+  lovrAudioStreamAppendRawBlob(stream, &sound->blob);
+}
+
 void lovrAudioStreamRewind(AudioStream* stream) {
+  lovrAssert(stream->decoder, "Can't rewind raw stream");
   stb_vorbis* decoder = (stb_vorbis*) stream->decoder;
   stb_vorbis_seek_start(decoder);
 }
 
 void lovrAudioStreamSeek(AudioStream* stream, size_t sample) {
+  lovrAssert(stream->decoder, "Can't seek raw stream");
   stb_vorbis* decoder = (stb_vorbis*) stream->decoder;
   stb_vorbis_seek(decoder, (int) sample);
 }
 
 size_t lovrAudioStreamTell(AudioStream* stream) {
+  lovrAssert(stream->decoder, "No position available in raw stream");
   stb_vorbis* decoder = (stb_vorbis*) stream->decoder;
   return stb_vorbis_get_sample_offset(decoder);
 }
--- src/modules/data/audioStream.h
+++ src/modules/data/audioStream.h
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include <stddef.h>
+#include "core/arr.h"
 
 #pragma once
 
 struct Blob;
+struct SoundData;
 
 typedef struct AudioStream {
   uint32_t bitDepth;
@@ -12,14 +14,19 @@ typedef struct AudioStream {
   size_t samples;
   size_t bufferSize;
   void* buffer;
-  void* decoder;
+  void* decoder; // null if stream is raw
   struct Blob* blob;
+  arr_t(struct Blob*) queuedRawBuffers;
 } AudioStream;
 
 AudioStream* lovrAudioStreamInit(AudioStream* stream, struct Blob* blob, size_t bufferSize);
 #define lovrAudioStreamCreate(...) lovrAudioStreamInit(lovrAlloc(AudioStream), __VA_ARGS__)
+AudioStream* lovrAudioStreamInitRaw(AudioStream* stream, size_t bufferSize, int channelCount, int sampleRate);
+#define lovrAudioStreamCreateRaw(...) lovrAudioStreamInitRaw(lovrAlloc(AudioStream), __VA_ARGS__)
 void lovrAudioStreamDestroy(void* ref);
 size_t lovrAudioStreamDecode(AudioStream* stream, int16_t* destination, size_t size);
+void lovrAudioStreamAppendRawBlob(AudioStream* stream, struct Blob* blob);
+void lovrAudioStreamAppendRawSound(AudioStream* stream, struct SoundData* sound);
 void lovrAudioStreamRewind(AudioStream* stream);
 void lovrAudioStreamSeek(AudioStream* stream, size_t sample);
 size_t lovrAudioStreamTell(AudioStream* stream);
