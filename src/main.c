42c85c39fb822885668fa5b7ef08d3aa9a6531ae
bjorn <tie.372@gmail.com>
2020-01-08 23:56:58 -0800

    rm 1ms sleep from main loop;
    It's causing massive problems on windows, and sleeping for zero ms
    is good enough to relinquish CPU time to the OS.  VR APIs also do
    their own sleeping.

--- src/main.c
+++ src/main.c
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
     return 0;
 #else
     while (lua_resume(T, 0) == LUA_YIELD) {
-      lovrPlatformSleep(.001);
+      lovrPlatformSleep(0.);
     }
 
     restart = lua_type(T, -1) == LUA_TSTRING && !strcmp(lua_tostring(T, -1), "restart");
