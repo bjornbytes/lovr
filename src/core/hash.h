08577ee91b9acc2d6bae6a07a1fa7e097840a9c2
bjorn <tie.372@gmail.com>
2020-01-23 11:01:27 -0800

    Fix cast-qual warning;

--- src/core/hash.h
+++ src/core/hash.h
@@ -5,7 +5,7 @@
 
 // FNV1a
 static LOVR_INLINE uint64_t hash64(const void* data, size_t length) {
-  const uint8_t* bytes = (uint8_t *)data;
+  const uint8_t* bytes = (const uint8_t*) data;
   uint64_t hash = 0xcbf29ce484222325;
   for (size_t i = 0; i < length; i++) {
     hash = (hash ^ bytes[i]) * 0x100000001b3;
