1eb4070c92053ca70f691d59a73979a71751b66d
bjorn <tie.372@gmail.com>
2020-05-19 12:08:24 -0600

    Mixing;

--- src/api/l_audio_source.c
+++ src/api/l_audio_source.c
@@ -19,9 +19,24 @@ static int l_lovrSourceSetLooping(lua_State* L) {
   return 0;
 }
 
+static int l_lovrSourceGetVolume(lua_State* L) {
+  Source* source = luax_checktype(L, 1, Source);
+  lua_pushnumber(L, lovrSourceGetVolume(source));
+  return 1;
+}
+
+static int l_lovrSourceSetVolume(lua_State* L) {
+  Source* source = luax_checktype(L, 1, Source);
+  float volume = luax_checkfloat(L, 2);
+  lovrSourceSetVolume(source, volume);
+  return 0;
+}
+
 const luaL_Reg lovrSource[] = {
   { "play", l_lovrSourcePlay },
   { "isLooping", l_lovrSourceIsLooping },
   { "setLooping", l_lovrSourceSetLooping },
+  { "getVolume", l_lovrSourceGetVolume },
+  { "setVolume", l_lovrSourceSetVolume },
   { NULL, NULL }
 };
