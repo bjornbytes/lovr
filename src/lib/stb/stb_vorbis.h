eeb5aef235f57ae6508ee8a4f30382a0732389c9
bjorn <tie.372@gmail.com>
2020-05-19 14:58:09 -0600

    Remove integer conversion APIs from stb_vorbis;

--- src/lib/stb/stb_vorbis.h
+++ src/lib/stb/stb_vorbis.h
@@ -1,5 +1,6 @@
 #define STB_VORBIS_NO_PUSHDATA_API
 #define STB_VORBIS_NO_STDIO
+#define STB_VORBIS_NO_INTEGER_CONVERSION
 
 //////////////////////////////////////////////////////////////////////////////
 //
