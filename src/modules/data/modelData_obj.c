ee95e6c6bdc95809cbe85839c2feacd14502939b
bjorn <tie.372@gmail.com>
2020-07-31 14:59:10 -0600

    Refactor OBJ model loading;
    It no longer uses sscanf and no longer overflows its buffer.

--- src/modules/data/modelData_obj.c
+++ src/modules/data/modelData_obj.c
@@ -6,7 +6,6 @@
 #include "core/map.h"
 #include "core/ref.h"
 #include "core/util.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
 #include <ctype.h>
@@ -23,20 +22,33 @@ typedef arr_t(objGroup) arr_group_t;
 
 #define STARTS_WITH(a, b) !strncmp(a, b, strlen(b))
 
-static void parseMtl(char* path, ModelDataIO* io, arr_texturedata_t* textures, arr_material_t* materials, map_t* names, char* base) {
-  size_t length = 0;
-  char* data = io(path, &length);
-  lovrAssert(data && length > 0, "Unable to read mtl from '%s'", path);
-  char* s = data;
+static uint32_t nomu32(char* s, char** end) {
+  uint32_t n = 0;
+  while (isdigit(*s)) { n = 10 * n + (*s++ - '0'); }
+  *end = s;
+  return n;
+}
 
-  while (length > 0) {
-    int lineLength = 0;
+static void parseMtl(char* path, char* base, ModelDataIO* io, arr_texturedata_t* textures, arr_material_t* materials, map_t* names) {
+  size_t size = 0;
+  char* p = io(path, &size);
+  lovrAssert(p && size > 0, "Unable to read mtl from '%s'", path);
+  char* data = p;
 
-    if (STARTS_WITH(s, "newmtl ")) {
-      char name[128];
-      size_t length = sscanf(s + 7, "%s\n%n", name, &lineLength);
-      lovrAssert(length > 0, "Bad OBJ: Expected a material name");
-      map_set(names, hash64(name, length), materials->length);
+  while (size > 0) {
+    while (size > 0 && (*data == ' ' || *data == '\t')) data++, size--;
+    char* newline = memchr(data, '\n', size);
+    if (*data == '#') goto next;
+
+    char line[1024];
+    size_t length = newline ? newline - data : size;
+    while (length > 0 && (data[length - 1] == '\r' || data[length - 1] == '\t' || data[length - 1] == ' ')) length--;
+    lovrAssert(length < sizeof(line), "OBJ MTL line length is too long (max is %d)", sizeof(line) - 1);
+    memcpy(line, data, length);
+    line[length] = '\0';
+
+    if (STARTS_WITH(line, "newmtl ")) {
+      map_set(names, hash64(line + 7, length - 7), materials->length);
       arr_push(materials, ((ModelMaterial) {
         .scalars[SCALAR_METALNESS] = 1.f,
         .scalars[SCALAR_ROUGHNESS] = 1.f,
@@ -44,26 +56,24 @@ static void parseMtl(char* path, ModelDataIO* io, arr_texturedata_t* textures, a
         .colors[COLOR_EMISSIVE] = { 0.f, 0.f, 0.f, 0.f }
       }));
       memset(&materials->data[materials->length - 1].textures, 0xff, MAX_MATERIAL_TEXTURES * sizeof(int));
-    } else if (STARTS_WITH(s, "Kd")) {
+    } else if (line[0] == 'K' && line[1] == 'd' && line[2] == ' ') {
       float r, g, b;
-      int count = sscanf(s + 2, "%f %f %f\n%n", &r, &g, &b, &lineLength);
-      lovrAssert(count == 3, "Bad OBJ: Expected 3 components for diffuse color");
+      char* s = line + 3;
+      r = strtof(s, &s);
+      g = strtof(s, &s);
+      b = strtof(s, &s);
       ModelMaterial* material = &materials->data[materials->length - 1];
       material->colors[COLOR_DIFFUSE] = (Color) { r, g, b, 1.f };
-    } else if (STARTS_WITH(s, "map_Kd")) {
+    } else if (STARTS_WITH(line, "map_Kd ")) {
+      lovrAssert(base - path + (length - 7) < 1024, "Bad OBJ: Material texture filename is too long");
+      memcpy(base, line + 7, length - 7);
+      base[length - 7] = '\0';
 
-      // Read file
-      char filename[128];
-      bool hasFilename = sscanf(s + 7, "%s\n%n", filename, &lineLength);
-      lovrAssert(hasFilename, "Bad OBJ: Expected a texture filename");
-      char path[1024];
-      snprintf(path, sizeof(path), "%s%s", base, filename);
-      size_t size = 0;
-      void* data = io(path, &size);
-      lovrAssert(data && size > 0, "Unable to read texture from %s", path);
-      Blob* blob = lovrBlobCreate(data, size, NULL);
+      size_t imageSize = 0;
+      void* image = io(path, &imageSize);
+      lovrAssert(image && imageSize > 0, "Unable to read texture from %s", path);
+      Blob* blob = lovrBlobCreate(image, imageSize, NULL);
 
-      // Load texture, assign to material
       TextureData* texture = lovrTextureDataCreateFromBlob(blob, true);
       lovrAssert(materials->length > 0, "Tried to set a material property without declaring a material first");
       ModelMaterial* material = &materials->data[materials->length - 1];
@@ -72,29 +82,20 @@ static void parseMtl(char* path, ModelDataIO* io, arr_texturedata_t* textures, a
       material->wraps[TEXTURE_DIFFUSE] = (TextureWrap) { .s = WRAP_REPEAT, .t = WRAP_REPEAT };
       arr_push(textures, texture);
       lovrRelease(Blob, blob);
-    } else {
-      char* newline = memchr(s, '\n', length);
-      lineLength = newline - s + 1;
     }
 
-    s += lineLength;
-    length -= lineLength;
-    while (length && (*s == ' ' || *s == '\t')) length--, s++;
+    next:
+    if (!newline) break;
+    size -= newline - data + 1;
+    data = newline + 1;
   }
 
-  free(data);
+  free(p);
 }
 
 ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO* io) {
   char* data = (char*) source->data;
-  size_t length = source->size;
-
-  if (!memchr(data, '\n', length)) {
-    return NULL;
-  }
-
-  float min[4] = { FLT_MAX };
-  float max[4] = { FLT_MIN };
+  size_t size = source->size;
 
   arr_group_t groups;
   arr_texturedata_t textures;
@@ -120,114 +121,119 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO* io)
 
   arr_push(&groups, ((objGroup) { .material = -1 }));
 
-  char base[1024];
-  lovrAssert(strlen(source->name) < sizeof(base), "OBJ filename is too long");
-  strcpy(base, source->name);
-  char* slash = strrchr(base, '/');
-  char* root = slash ? (slash + 1) : base;
-  *root = '\0';
+  char path[1024];
+  size_t pathLength = strlen(source->name);
+  lovrAssert(pathLength < sizeof(path), "OBJ filename is too long");
+  memcpy(path, source->name, pathLength);
+  path[pathLength] = '\0';
+  char* slash = strrchr(path, '/');
+  char* base = slash ? (slash + 1) : path;
+  size_t baseLength = base - path;
+  *base = '\0';
 
-  while (length > 0) {
-    int lineLength = 0;
+  while (size > 0) {
+    while (size > 0 && (*data == ' ' || *data == '\t')) data++, size--;
+    char* newline = memchr(data, '\n', size);
+    if (*data == '#') goto next;
 
-    if (STARTS_WITH(data, "v ")) {
-      float x, y, z;
-      int count = sscanf(data + 2, "%f %f %f\n%n", &x, &y, &z, &lineLength);
-      lovrAssert(count == 3, "Bad OBJ: Expected 3 coordinates for vertex position");
-      min[0] = MIN(min[0], x);
-      max[0] = MAX(max[0], x);
-      min[1] = MIN(min[1], y);
-      max[1] = MAX(max[1], y);
-      min[2] = MIN(min[2], z);
-      max[2] = MAX(max[2], z);
-      arr_append(&positions, ((float[3]) { x, y, z }), 3);
-    } else if (STARTS_WITH(data, "vn ")) {
-      float x, y, z;
-      int count = sscanf(data + 3, "%f %f %f\n%n", &x, &y, &z, &lineLength);
-      lovrAssert(count == 3, "Bad OBJ: Expected 3 coordinates for vertex normal");
-      arr_append(&normals, ((float[3]) { x, y, z }), 3);
-    } else if (STARTS_WITH(data, "vt ")) {
-      float u, v;
-      int count = sscanf(data + 3, "%f %f\n%n", &u, &v, &lineLength);
-      lovrAssert(count == 2, "Bad OBJ: Expected 2 coordinates for texture coordinate");
-      arr_append(&uvs, ((float[2]) { u, v }), 2);
-    } else if (STARTS_WITH(data, "f ")) {
-      char* s = data + 2;
-      for (int i = 0; i < 3; i++) {
-        char terminator = i == 2 ? '\n' : ' ';
-        char* space = strchr(s, terminator);
-        if (space) {
-          uint64_t hash = hash64(s, space - s);
-          uint64_t index = map_get(&vertexMap, hash);
-          if (index != MAP_NIL) {
-            arr_push(&indexBlob, index);
+    char line[1024];
+    size_t length = newline ? newline - data : size;
+    while (length > 0 && (data[length - 1] == '\r' || data[length - 1] == '\t' || data[length - 1] == ' ')) length--;
+    lovrAssert(length < sizeof(line), "OBJ line length is too long (max is %d)", sizeof(line) - 1);
+    memcpy(line, data, length);
+    line[length] = '\0';
+
+    if (line[0] == 'v' && line[1] == ' ') {
+      float v[3];
+      char* s = line + 2;
+      v[0] = strtof(s, &s);
+      v[1] = strtof(s, &s);
+      v[2] = strtof(s, &s);
+      arr_append(&positions, v, 3);
+    } else if (line[0] == 'v' && line[1] == 'n' && line[2] == ' ') {
+      float vn[3];
+      char* s = line + 3;
+      vn[0] = strtof(s, &s);
+      vn[1] = strtof(s, &s);
+      vn[2] = strtof(s, &s);
+      arr_append(&normals, vn, 3);
+    } else if (line[0] == 'v' && line[1] == 't' && line[2] == ' ') {
+      float vt[2];
+      char* s = line + 3;
+      vt[0] = strtof(s, &s);
+      vt[1] = strtof(s, &s);
+      arr_append(&uvs, vt, 2);
+    } else if (line[0] == 'f' && line[1] == ' ') {
+      char* s = line + 2;
+      for (size_t i = 0; i < 3; i++) {
+
+        // Find first number/slash
+        while (*s && !(*s >= '/' && *s <= '9')) s++;
+
+        // Find next non-number/non-slash
+        char* t = s;
+        while (*t && *t >= '/' && *t <= '9') t++;
+
+        // If the vertex already exists, add its index and skip
+        uint64_t hash = hash64(s, t - s);
+        uint64_t index = map_get(&vertexMap, hash);
+        if (index != MAP_NIL) {
+          arr_push(&indexBlob, index);
+          s = t;
+          continue;
+        }
+
+        uint32_t v = 0;
+        uint32_t vt = 0;
+        uint32_t vn = 0;
+        v = nomu32(s, &s);
+        lovrAssert(v > 0, "Bad OBJ: Expected positive number for face vertex position index");
+
+        // Handle v//vn, v/vt, v/vt/vtn, and v
+        if (s[0] == '/') {
+          if (s[1] == '/') {
+            vn = nomu32(s + 2, &s);
           } else {
-            int v, vt, vn;
-            uint64_t newIndex = vertexBlob.length / 8;
-            arr_push(&indexBlob, newIndex);
-            map_set(&vertexMap, hash, newIndex);
-
-            // Can be improved
-            if (sscanf(s, "%d/%d/%d", &v, &vt, &vn) == 3) {
-              arr_append(&vertexBlob, positions.data + 3 * (v - 1), 3);
-              arr_append(&vertexBlob, normals.data + 3 * (vn - 1), 3);
-              arr_append(&vertexBlob, uvs.data + 2 * (vt - 1), 2);
-            } else if (sscanf(s, "%d//%d", &v, &vn) == 2) {
-              arr_append(&vertexBlob, positions.data + 3 * (v - 1), 3);
-              arr_append(&vertexBlob, normals.data + 3 * (vn - 1), 3);
-              arr_append(&vertexBlob, ((float[2]) { 0 }), 2);
-            } else if (sscanf(s, "%d/%d", &v, &vt) == 2) {
-              arr_append(&vertexBlob, positions.data + 3 * (v - 1), 3);
-              arr_append(&vertexBlob, ((float[3]) { 0 }), 3);
-              arr_append(&vertexBlob, uvs.data + 2 * (vt - 1), 2);
-            } else if (sscanf(s, "%d", &v) == 1) {
-              arr_append(&vertexBlob, positions.data + 3 * (v - 1), 3);
-              arr_append(&vertexBlob, ((float[5]) { 0 }), 5);
-            } else {
-              lovrThrow("Bad OBJ: Unknown face format");
+            vt = nomu32(s + 1, &s);
+            if (s[0] == '/') {
+              vn = nomu32(s + 1, &s);
             }
           }
-          s = space + 1;
         }
+
+        float empty[3] = { 0.f };
+        arr_push(&indexBlob, vertexBlob.length / 8);
+        map_set(&vertexMap, hash, vertexBlob.length / 8);
+        arr_append(&vertexBlob, positions.data + 3 * (v - 1), 3);
+        arr_append(&vertexBlob, vn > 0 ? (normals.data + 3 * (vn - 1)) : empty, 3);
+        arr_append(&vertexBlob, vt > 0 ? (uvs.data + 2 * (vt - 1)) : empty, 2);
+
+        s = t;
       }
       groups.data[groups.length - 1].count += 3;
-      lineLength = s - data;
-    } else if (STARTS_WITH(data, "mtllib ")) {
-      char filename[1024];
-      bool hasName = sscanf(data + 7, "%1024s\n%n", filename, &lineLength);
-      lovrAssert(hasName, "Bad OBJ: Expected filename after mtllib");
-      char path[1024];
-      snprintf(path, sizeof(path), "%s%s", base, filename);
-      parseMtl(path, io, &textures, &materials, &materialMap, base);
-    } else if (STARTS_WITH(data, "usemtl ")) {
-      char name[128];
-      uint64_t length = sscanf(data + 7, "%s\n%n", name, &lineLength);
-      uint64_t material = map_get(&materialMap, hash64(name, length));
-      lovrAssert(length > 0, "Bad OBJ: Expected a valid material name");
-
-      // If the last group didn't have any faces, just reuse it, otherwise make a new group
+    } else if (STARTS_WITH(line, "mtllib ")) {
+      const char* filename = line + 7;
+      size_t filenameLength = strlen(filename);
+      lovrAssert(baseLength + filenameLength < sizeof(path), "Bad OBJ: Material filename is too long");
+      memcpy(path + baseLength, filename, filenameLength);
+      path[baseLength + filenameLength] = '\0';
+      parseMtl(path, base, io, &textures, &materials, &materialMap);
+    } else if (STARTS_WITH(line, "usemtl ")) {
+      uint64_t index = map_get(&materialMap, hash64(line + 7, length - 7));
+      int material = index == MAP_NIL ? -1 : index;
       objGroup* group = &groups.data[groups.length - 1];
       if (group->count > 0) {
-        int start = group->start + group->count; // Don't put this in the compound literal (realloc)
-        arr_push(&groups, ((objGroup) {
-          .material = material == MAP_NIL ? -1 : material,
-          .start = start,
-          .count = 0
-        }));
-      } else {
-        group->material = material == MAP_NIL ? -1 : material;
+        objGroup next = { .material = material, .start = group->start + group->count };
+        arr_push(&groups, next);
+      } else { // If the group doesn't have any faces yet, it's safe to modify its material
+        group->material = material;
       }
-    } else {
-      char* newline = memchr(data, '\n', length);
-      if (!newline) {
-        break;
-      }
-      lineLength = newline - data + 1;
     }
 
-    data += lineLength;
-    length -= lineLength;
-    while (length && (*data == ' ' || *data == '\t')) length--, data++;
+    next:
+    if (!newline) break;
+    size -= newline - data + 1;
+    data = newline + 1;
   }
 
   if (vertexBlob.length == 0 || indexBlob.length == 0) {
@@ -260,7 +266,21 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO* io)
 
   memcpy(model->textures, textures.data, model->textureCount * sizeof(TextureData*));
   memcpy(model->materials, materials.data, model->materialCount * sizeof(ModelMaterial));
-  model->materialMap = materialMap; // Copy by value, no need to free original, questionable
+  memcpy(model->materialMap.hashes, materialMap.hashes, materialMap.size * sizeof(uint64_t));
+  memcpy(model->materialMap.values, materialMap.values, materialMap.size * sizeof(uint64_t));
+
+  float min[4] = { FLT_MAX };
+  float max[4] = { FLT_MIN };
+
+  for (size_t i = 0; i < vertexBlob.length; i += 8) {
+    float* v = vertexBlob.data + i;
+    min[0] = MIN(min[0], v[0]);
+    max[0] = MAX(max[0], v[0]);
+    min[1] = MIN(min[1], v[1]);
+    max[1] = MAX(max[1], v[1]);
+    min[2] = MIN(min[2], v[2]);
+    max[2] = MAX(max[2], v[2]);
+  }
 
   model->attributes[0] = (ModelAttribute) {
     .buffer = 0,
@@ -330,6 +350,7 @@ ModelData* lovrModelDataInitObj(ModelData* model, Blob* source, ModelDataIO* io)
   arr_free(&groups);
   arr_free(&textures);
   arr_free(&materials);
+  map_free(&materialMap);
   map_free(&vertexMap);
   arr_free(&positions);
   arr_free(&normals);
