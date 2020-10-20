5210103b911ad9d4c42af4ce16cf447fc67133eb
bjorn <tie.372@gmail.com>
2020-10-20 12:18:59 -0600

    core/gpu; Transform stack;

--- src/modules/graphics/graphics.h
+++ src/modules/graphics/graphics.h
@@ -3,5 +3,13 @@
 
 #pragma once
 
-bool lovrGraphicsInit();
+bool lovrGraphicsInit(bool debug);
 void lovrGraphicsDestroy(void);
+
+void lovrGraphicsPush(void);
+void lovrGraphicsPop(void);
+void lovrGraphicsOrigin(void);
+void lovrGraphicsTranslate(float* translation);
+void lovrGraphicsRotate(float* rotation);
+void lovrGraphicsScale(float* scale);
+void lovrGraphicsMatrixTransform(float* transform);
