842efe36c7b85ec29c6d1e2522922e3de0e7234c
bjornbytes <tie.372@gmail.com>
2020-11-21 14:09:51 -0700

    Handle openvr-specific warnings more precisely;
    Instead of disabling a warning for the entire project, it is only
    disabled while processing the OpenVR header.

--- src/modules/headset/headset_openvr.c
+++ src/modules/headset/headset_openvr.c
@@ -16,7 +16,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #undef EXTERN_C
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wtypedef-redefinition"
 #include <openvr_capi.h>
+#pragma clang diagnostic pop
 
 // From openvr_capi.h
 extern intptr_t VR_InitInternal(EVRInitError *peError, EVRApplicationType eType);
