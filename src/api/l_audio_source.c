533939e6739ac110c957cd15bfa313773316a936
bjorn <tie.372@gmail.com>
2020-11-21 20:42:15 -0700

    Source:getDuration; Source:get/setTime;

--- src/api/l_audio_source.c
+++ src/api/l_audio_source.c
@@ -1,5 +1,6 @@
 #include "api.h"
 #include "audio/audio.h"
+#include "data/soundData.h"
 
 static int l_lovrSourcePlay(lua_State* L) {
   Source* source = luax_checktype(L, 1, Source);
@@ -50,6 +51,50 @@ static int l_lovrSourceSetVolume(lua_State* L) {
   return 0;
 }
 
+static int l_lovrSourceGetDuration(lua_State* L) {
+  Source* source = luax_checktype(L, 1, Source);
+  TimeUnit units = luax_checkenum(L, 2, TimeUnit, "seconds");
+  SoundData* sound = lovrSourceGetSoundData(source);
+
+  if (units == UNIT_SECONDS) {
+    lua_pushnumber(L, (double) sound->frames / sound->sampleRate);
+  } else {
+    lua_pushinteger(L, sound->frames);
+  }
+
+  return 1;
+}
+
+static int l_lovrSourceGetTime(lua_State* L) {
+  Source* source = luax_checktype(L, 1, Source);
+  TimeUnit units = luax_checkenum(L, 2, TimeUnit, "seconds");
+  uint32_t offset = lovrSourceGetTime(source);
+
+  if (units == UNIT_SECONDS) {
+    SoundData* sound = lovrSourceGetSoundData(source);
+    lua_pushnumber(L, (double) offset / sound->sampleRate);
+  } else {
+    lua_pushinteger(L, offset);
+  }
+
+  return 1;
+}
+
+static int l_lovrSourceSetTime(lua_State* L) {
+  Source* source = luax_checktype(L, 1, Source);
+  TimeUnit units = luax_checkenum(L, 3, TimeUnit, "seconds");
+
+  if (units == UNIT_SECONDS) {
+    double seconds = luaL_checknumber(L, 2);
+    SoundData* sound = lovrSourceGetSoundData(source);
+    lovrSourceSetTime(source, (uint32_t) (seconds * sound->sampleRate + .5));
+  } else {
+    lovrSourceSetTime(source, luaL_checkinteger(L, 2));
+  }
+
+  return 0;
+}
+
 const luaL_Reg lovrSource[] = {
   { "play", l_lovrSourcePlay },
   { "pause", l_lovrSourcePause },
@@ -59,5 +104,8 @@ const luaL_Reg lovrSource[] = {
   { "setLooping", l_lovrSourceSetLooping },
   { "getVolume", l_lovrSourceGetVolume },
   { "setVolume", l_lovrSourceSetVolume },
+  { "getDuration", l_lovrSourceGetDuration },
+  { "getTime", l_lovrSourceGetTime },
+  { "setTime", l_lovrSourceSetTime },
   { NULL, NULL }
 };
