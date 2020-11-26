e27b2f4e34491376a9735698f385b4cbca313463
Nevyn Bengtsson <nevyn@alloverse.com>
2020-11-26 22:14:02 +0100

    SoundData:getBlob

--- src/api/l_data_soundData.c
+++ src/api/l_data_soundData.c
@@ -1,6 +1,15 @@
 #include "api.h"
 #include "data/soundData.h"
+#include "data/blob.h"
+
+static int l_lovrSoundDataGetBlob(lua_State* L) {
+  SoundData* soundData = luax_checktype(L, 1, SoundData);
+  Blob* blob = soundData->blob;
+  luax_pushtype(L, Blob, blob);
+  return 1;
+}
+
 
 const luaL_Reg lovrSoundData[] = {
-  { NULL, NULL }
+  { "getBlob", l_lovrSoundDataGetBlob }
 };
