5631801fff14e624a9b50a51281ec6fd92efb6c3
bjornbytes <tie.372@gmail.com>
2020-11-27 18:19:43 -0700

    gpu: TEXTURE_FEATURE -> FORMAT_FEATURE;

--- src/core/gpu_vk.c
+++ src/core/gpu_vk.c
@@ -491,14 +491,14 @@ bool gpu_init(gpu_config* config) {
       uint32_t blitMask = VK_FORMAT_FEATURE_BLIT_SRC_BIT | VK_FORMAT_FEATURE_BLIT_DST_BIT;
       uint32_t vkFeatures = formatProperties.optimalTilingFeatures;
       uint32_t gpuFeatures = 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) ? GPU_TEXTURE_FEATURE_SAMPLE : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT) ? GPU_TEXTURE_FEATURE_CANVAS_COLOR : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) ? GPU_TEXTURE_FEATURE_CANVAS_DEPTH : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BLEND_BIT) ? GPU_TEXTURE_FEATURE_BLEND : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) ? GPU_TEXTURE_FEATURE_FILTER : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) ? GPU_TEXTURE_FEATURE_STORAGE : 0;
-      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_ATOMIC_BIT) ? GPU_TEXTURE_FEATURE_ATOMIC : 0;
-      gpuFeatures |= (vkFeatures & blitMask) == blitMask ? GPU_TEXTURE_FEATURE_BLIT : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT) ? GPU_FORMAT_FEATURE_SAMPLE : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT) ? GPU_FORMAT_FEATURE_CANVAS_COLOR : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) ? GPU_FORMAT_FEATURE_CANVAS_DEPTH : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BLEND_BIT) ? GPU_FORMAT_FEATURE_BLEND : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT) ? GPU_FORMAT_FEATURE_FILTER : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) ? GPU_FORMAT_FEATURE_STORAGE : 0;
+      gpuFeatures |= (vkFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_ATOMIC_BIT) ? GPU_FORMAT_FEATURE_ATOMIC : 0;
+      gpuFeatures |= (vkFeatures & blitMask) == blitMask ? GPU_FORMAT_FEATURE_BLIT : 0;
       config->features.formats[i] = gpuFeatures;
     }
 
