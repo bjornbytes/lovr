b5eb242595815ca6106097d904db3fcd34728ca9
Nevyn Bengtsson <nevyn@alloverse.com>
2020-12-07 16:50:32 +0100

    Audio permissions seem to work without AppCompat

--- src/resources/Activity_vrapi.java
+++ src/resources/Activity_vrapi.java
@@ -2,12 +2,11 @@ package org.lovr.app;
 import android.app.NativeActivity;
 import android.Manifest;
 import android.app.NativeActivity;
-import android.support.v4.app.ActivityCompat;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
-public class Activity extends NativeActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+public class Activity extends NativeActivity {
   static {
     System.loadLibrary("lovr");
     System.loadLibrary("vrapi");
@@ -26,24 +25,27 @@ public class Activity extends NativeActivity implements ActivityCompat.OnRequest
   {
     if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
     {
+      Log.i("LOVR", "RECORD_AUDIO granted.");
       lovrPermissionEvent(0, true);
     }
     else
     {
+      Log.i("LOVR", "RECORD_AUDIO rejected.");
       lovrPermissionEvent(0, false);
     }
   }
 
   private void requestAudioCapturePermission()
   {
-    int existingPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+    int existingPermission = checkSelfPermission(Manifest.permission.RECORD_AUDIO);
     if(existingPermission != PackageManager.PERMISSION_GRANTED)
     {
       Log.i("LOVR", "Asking for RECORD_AUDIO permissions.");
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+      requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
     }
     else
     {
+      Log.i("LOVR", "RECORD_AUDIO already permitted.");
       lovrPermissionEvent(0, true);
     }
   }
