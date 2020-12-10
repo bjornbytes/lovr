3afb2b87984a753a24ebb29ebd988cd9b7e3cf56
Nevyn Bengtsson <nevyn@alloverse.com>
2020-12-10 13:29:42 +0100

    lua API for SoundData:append

--- src/api/l_data_soundData.c
+++ src/api/l_data_soundData.c
@@ -9,8 +9,23 @@ static int l_lovrSoundDataGetBlob(lua_State* L) {
   return 1;
 }
 
+static int l_lovrSoundDataAppend(lua_State* L) {
+  SoundData* soundData = luax_checktype(L, 1, SoundData);
+  Blob* blob = luax_totype(L, 2, Blob);
+  SoundData* sound = luax_totype(L, 2, SoundData);
+  lovrAssert(blob || sound, "Invalid blob appended");
+  size_t appendedSamples = false;
+  if (sound) {
+    appendedSamples = lovrSoundDataStreamAppendSound(soundData, sound);
+  } else if (blob) {
+    appendedSamples = lovrSoundDataStreamAppendBlob(soundData, blob);
+  }
+  lua_pushinteger(L, appendedSamples);
+  return 1;
+}
 
 const luaL_Reg lovrSoundData[] = {
   { "getBlob", l_lovrSoundDataGetBlob },
+  { "append", l_lovrSoundDataAppend },
   { NULL, NULL }
 };
