0c03be1090ac5f66ad365871f7a6ea5486523d3a
bjorn <tie.372@gmail.com>
2021-01-07 14:35:28 -0700

    tup updates;
    - rm json and enet (they're plugins now)
    - ignore a warning in stb_image

--- src/lib/stb/stb_image.c
+++ src/lib/stb/stb_image.c
@@ -4,4 +4,7 @@
 #define STBI_ONLY_PNG
 #define STBI_ONLY_HDR
 #define STBI_ASSERT(x)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-compare"
 #include "stb_image.h"
+#pragma clang diagnostic pop
