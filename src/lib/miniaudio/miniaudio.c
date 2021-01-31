13e100275e61dc469a6dc33fe6d5781d65a228c6
bjorn <tie.372@gmail.com>
2021-01-31 13:34:36 -0700

    Fix style issues;

--- src/lib/miniaudio/miniaudio.c
+++ src/lib/miniaudio/miniaudio.c
@@ -1,7 +1,6 @@
 #define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_DECODING
 #ifdef ANDROID
-    #define MA_NO_RUNTIME_LINKING
+#define MA_NO_RUNTIME_LINKING
 #endif
-//#define MA_DEBUG_OUTPUT
 #include "miniaudio.h"
