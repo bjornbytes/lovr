ffa63e9912e4dd1dbf935cf9ef4617ae180214c4
mcc <andi.m.mcclure@gmail.com>
2021-02-19 23:06:21 -0500

    Allow the creation of a (potentially infinite-length) Sound created by a dynamically run callback

--- src/modules/data/sound.h
+++ src/modules/data/sound.h
@@ -18,9 +18,17 @@ typedef enum {
 } ChannelLayout;
 
 typedef struct Sound Sound;
+
+typedef uint32_t (SoundCallback)(Sound* sound, uint32_t offset, uint32_t count, void* data);
+typedef void (SoundDestroyCallback)(Sound* sound);
+
+// Can pass as the maxFrames argument to lovrSoundCreateFromCallback
+#define LOVR_SOUND_ENDLESS 0xFFFFFFFF
+
 Sound* lovrSoundCreateRaw(uint32_t frames, SampleFormat format, ChannelLayout channels, uint32_t sampleRate, struct Blob* data);
 Sound* lovrSoundCreateStream(uint32_t frames, SampleFormat format, ChannelLayout channels, uint32_t sampleRate);
 Sound* lovrSoundCreateFromFile(struct Blob* blob, bool decode);
+Sound* lovrSoundCreateFromCallback(SoundCallback read, void *callbackMemo, SoundDestroyCallback callbackDataDestroy, SampleFormat format, uint32_t sampleRate, ChannelLayout channels, uint32_t maxFrames);
 void lovrSoundDestroy(void* ref);
 struct Blob* lovrSoundGetBlob(Sound* sound);
 SampleFormat lovrSoundGetFormat(Sound* sound);
@@ -34,3 +42,4 @@ bool lovrSoundIsStream(Sound* sound);
 uint32_t lovrSoundRead(Sound* sound, uint32_t offset, uint32_t count, void* data);
 uint32_t lovrSoundWrite(Sound* sound, uint32_t offset, uint32_t count, const void* data);
 uint32_t lovrSoundCopy(Sound* src, Sound* dst, uint32_t frames, uint32_t srcOffset, uint32_t dstOffset);
+void *lovrSoundGetCallbackMemo(Sound *sound);
