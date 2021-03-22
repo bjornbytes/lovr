5ef5ac5f22a1759772fc79954ba2210b2f3492e8
bjorn <tie.372@gmail.com>
2021-03-22 12:04:15 -0600

    I am once again fixing the mixer EOF case;

--- src/modules/audio/audio.c
+++ src/modules/audio/audio.c
@@ -129,8 +129,8 @@ static void onPlayback(ma_device* device, void* out, const void* in, uint32_t co
           } else {
             source->offset = 0;
             source->playing = false;
-            memset(aux + framesConverted * channels, 0, framesToConvert * channels * sizeof(float));
-            src = aux;
+            src = source->converter ? aux : raw;
+            memset(src + framesConverted * channels, 0, framesToConvert * channels * sizeof(float));
             break;
           }
         }
