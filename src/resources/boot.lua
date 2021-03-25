88e280340334e27b910b84b5d6fbb2d7598bc830
mcc <andi.m.mcclure@gmail.com>
2021-03-25 16:34:20 -0400

    Allow boot.lua to run in situations where the debug object is absent

--- src/resources/boot.lua
+++ src/resources/boot.lua
@@ -238,7 +238,9 @@ end
 
 function lovr.errhand(message, traceback)
   message = tostring(message)
-  message = message .. formatTraceback(traceback or debug.traceback('', 4))
+  if debug then
+    message = message .. formatTraceback(traceback or debug.traceback('', 4))
+  end
   print('Error:\n' .. message)
   if not lovr.graphics then return function() return 1 end end
 
