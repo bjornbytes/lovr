3c6d63e76b891fff33176630e9035152cf061da0
bjorn <mail@bjornbyt.es>
2021-07-20 22:33:49 -0700

    Batch camera; Pure melancholy;

--- src/api/l_graphics_batch.c
+++ src/api/l_graphics_batch.c
@@ -1,5 +1,6 @@
 #include "api.h"
 #include "graphics/graphics.h"
+#include "core/maf.h"
 #include "core/util.h"
 #include <lua.h>
 #include <lauxlib.h>
@@ -28,6 +29,100 @@ static int l_lovrBatchGetCount(lua_State* L) {
 static int l_lovrBatchReset(lua_State* L) {
   Batch* batch = luax_checktype(L, 1, Batch);
   lovrBatchReset(batch);
+  return 0;
+}
+
+static int l_lovrBatchGetViewPose(lua_State* L) {
+  Batch* batch = luax_checktype(L, 1, Batch);
+  uint32_t view = luaL_checkinteger(L, 2) - 1;
+  lovrAssert(view < 6, "Invalid view index %d", view + 1);
+  if (lua_gettop(L) > 2) {
+    float* matrix = luax_checkvector(L, 3, V_MAT4, NULL);
+    bool invert = lua_toboolean(L, 4);
+    lovrBatchGetViewMatrix(batch, view, matrix);
+    if (!invert) mat4_invert(matrix);
+    lua_settop(L, 3);
+    return 1;
+  } else {
+    float matrix[16], angle, ax, ay, az;
+    lovrBatchGetViewMatrix(batch, view, matrix);
+    mat4_invert(matrix);
+    mat4_getAngleAxis(matrix, &angle, &ax, &ay, &az);
+    lua_pushnumber(L, matrix[12]);
+    lua_pushnumber(L, matrix[13]);
+    lua_pushnumber(L, matrix[14]);
+    lua_pushnumber(L, angle);
+    lua_pushnumber(L, ax);
+    lua_pushnumber(L, ay);
+    lua_pushnumber(L, az);
+    return 7;
+  }
+}
+
+static int l_lovrBatchSetViewPose(lua_State* L) {
+  Batch* batch = luax_checktype(L, 1, Batch);
+  uint32_t view = luaL_checkinteger(L, 2) - 1;
+  lovrAssert(view < 6, "Invalid view index %d", view + 1);
+  VectorType type;
+  float* p = luax_tovector(L, 3, &type);
+  if (p && type == V_MAT4) {
+    float matrix[16];
+    mat4_init(matrix, p);
+    bool inverted = lua_toboolean(L, 3);
+    if (!inverted) mat4_invert(matrix);
+    lovrBatchSetViewMatrix(batch, view, matrix);
+  } else {
+    int index = 3;
+    float position[4], orientation[4], matrix[16];
+    index = luax_readvec3(L, index, position, "vec3, number, or mat4");
+    index = luax_readquat(L, index, orientation, NULL);
+    mat4_fromQuat(matrix, orientation);
+    memcpy(matrix + 12, position, 3 * sizeof(float));
+    mat4_invert(matrix);
+    lovrBatchSetViewMatrix(batch, view, matrix);
+  }
+  return 0;
+}
+
+static int l_lovrBatchGetProjection(lua_State* L) {
+  Batch* batch = luax_checktype(L, 1, Batch);
+  uint32_t view = luaL_checkinteger(L, 2) - 1;
+  lovrAssert(view < 6, "Invalid view index %d", view + 1);
+  if (lua_gettop(L) > 2) {
+    float* matrix = luax_checkvector(L, 3, V_MAT4, NULL);
+    lovrBatchGetProjection(batch, view, matrix);
+    lua_settop(L, 3);
+    return 1;
+  } else {
+    float matrix[16], left, right, up, down;
+    lovrBatchGetProjection(batch, view, matrix);
+    mat4_getFov(matrix, &left, &right, &up, &down);
+    lua_pushnumber(L, left);
+    lua_pushnumber(L, right);
+    lua_pushnumber(L, up);
+    lua_pushnumber(L, down);
+    return 4;
+  }
+}
+
+static int l_lovrBatchSetProjection(lua_State* L) {
+  Batch* batch = luax_checktype(L, 1, Batch);
+  uint32_t view = luaL_checkinteger(L, 2) - 1;
+  lovrAssert(view < 6, "Invalid view index %d", view + 1);
+  if (lua_type(L, 3) == LUA_TNUMBER) {
+    float left = luax_checkfloat(L, 3);
+    float right = luax_checkfloat(L, 4);
+    float up = luax_checkfloat(L, 5);
+    float down = luax_checkfloat(L, 6);
+    float clipNear = luax_optfloat(L, 7, .1f);
+    float clipFar = luax_optfloat(L, 8, 100.f);
+    float matrix[16];
+    mat4_fov(matrix, left, right, up, down, clipNear, clipFar);
+    lovrBatchSetProjection(batch, view, matrix);
+  } else {
+    float* matrix = luax_checkvector(L, 2, V_MAT4, "mat4 or number");
+    lovrBatchSetProjection(batch, view, matrix);
+  }
   return 0;
 }
 
@@ -435,6 +530,10 @@ const luaL_Reg lovrBatch[] = {
   { "getCapacity", l_lovrBatchGetCapacity },
   { "getCount", l_lovrBatchGetCount },
   { "reset", l_lovrBatchReset },
+  { "getViewPose", l_lovrBatchGetViewPose },
+  { "setViewPose", l_lovrBatchSetViewPose },
+  { "getProjection", l_lovrBatchGetProjection },
+  { "setProjection", l_lovrBatchSetProjection },
   { "push", l_lovrBatchPush },
   { "pop", l_lovrBatchPop },
   { "origin", l_lovrBatchOrigin },
