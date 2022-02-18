ef41e06fc94ac1f704751a9ed418b132d7c47a1c
Josip Miskovic <josipmiskovic@gmail.com>
2022-02-18 20:05:02 +0100

    Add Vec2/3/4:angle(other)
    Functions to calculate the angle between two vectors. Angle is always
    positive. Implementations give the same result as this Lua code:
    
    ```lua
    local function lua_angle(v1, v2)
      return math.acos(v1:dot(v2) / (v1:length() * v2:length()))
    end
    ```
    
    If either vector is zero-length, the pi/2 value is returned.

--- src/core/maf.h
+++ src/core/maf.h
@@ -122,6 +122,15 @@ MAF vec3 vec3_max(vec3 v, const vec3 u) {
   return v;
 }
 
+MAF float vec3_angle(const vec3 v, const vec3 u) {
+  float denom = vec3_length(v) * vec3_length(u);
+  if (denom == 0.f) {
+    return (float) M_PI / 2;
+  } else {
+    return acosf(vec3_dot(v, u) / denom);
+  }
+}
+
 // quat
 
 MAF quat quat_set(quat q, float x, float y, float z, float w) {
