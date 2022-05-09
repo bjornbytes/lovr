91c48e57d536704b5420b70b099fccbf0b28acac
bjorn <mail@bjornbyt.es>
2022-05-09 12:41:21 -0700

    macOS: implement virtual memory functions;

--- src/core/os_macos.c
+++ src/core/os_macos.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <pwd.h>
+#include <sys/mman.h>
 #import <AVFoundation/AVFoundation.h>
 
 #include "os_glfw.h"
@@ -91,6 +92,18 @@ void os_request_permission(os_permission permission) {
 #error "Unsupported macOS target"
 #endif
   }
+}
+
+void* os_vm_init(size_t size) {
+  return mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+}
+
+bool os_vm_free(void* p, size_t size) {
+  return !munmap(p, size);
+}
+
+bool os_vm_commit(void* p, size_t size) {
+  return !mprotect(p, size, PROT_READ | PROT_WRITE);
 }
 
 void os_on_permission(fn_permission* callback) {
