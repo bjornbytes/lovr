adaef45e2fc58dce427a929625b23973c0ef88e2
bjorn <mail@bjornbyt.es>
2022-09-14 20:53:17 -0700

    tup: use -Os when invoking glslangValidator;
    Saves a bit of space.

--- Tupfile.lua
+++ Tupfile.lua
@@ -454,7 +454,7 @@ comp = 'etc/shaders/*.comp'
 
 function compileShaders(stage)
   pattern = 'etc/shaders/*.' .. stage
-  tup.foreach_rule(pattern, 'glslangValidator --quiet --target-env vulkan1.1 --vn lovr_shader_%B_' .. stage .. ' -o %o %f', '%f.h')
+  tup.foreach_rule(pattern, 'glslangValidator --quiet -Os --target-env vulkan1.1 --vn lovr_shader_%B_' .. stage .. ' -o %o %f', '%f.h')
 end
 
 compileShaders('vert')
