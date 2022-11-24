0c281bca505ce7e3cf84afdd1415037802e7ac21
bjorn <mail@bjornbyt.es>
2022-11-24 14:58:40 -0800

    Change default setProjection far plane to 0;
    It was accidentally left at 100, which will cause problems.

--- src/api/l_graphics_pass.c
+++ src/api/l_graphics_pass.c
@@ -188,7 +188,7 @@ static int l_lovrPassSetProjection(lua_State* L) {
     float up = luax_checkfloat(L, 5);
     float down = luax_checkfloat(L, 6);
     float clipNear = luax_optfloat(L, 7, .01f);
-    float clipFar = luax_optfloat(L, 8, 100.f);
+    float clipFar = luax_optfloat(L, 8, 0.f);
     float matrix[16];
     mat4_fov(matrix, left, right, up, down, clipNear, clipFar);
     lovrPassSetProjection(pass, view, matrix);
