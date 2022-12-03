2bbf3eec35d981d3068315c89af8c5a90958295c
bjorn <mail@bjornbyt.es>
2022-12-02 23:32:28 -0800

    webgpu: sampler;

--- src/core/gpu_webgpu.c
+++ src/core/gpu_webgpu.c
@@ -3,9 +3,59 @@
 #include <emscripten/html5_webgpu.h>
 #include <string.h>
 
+struct gpu_sampler {
+  WGPUSampler handle;
+};
+
+size_t gpu_sizeof_sampler(void) { return sizeof(gpu_sampler); }
+
 static struct {
   WGPUDevice device;
 } state;
+
+// Sampler
+
+bool gpu_sampler_init(gpu_sampler* sampler, gpu_sampler_info* info) {
+  static const WGPUFilterMode filters[] = {
+    [GPU_FILTER_NEAREST] = WGPUFilterMode_Nearest,
+    [GPU_FILTER_LINEAR] = WGPUFilterMode_Linear
+  };
+
+  static const WGPUAddressMode wraps[] = {
+    [GPU_WRAP_CLAMP] = WGPUAddressMode_ClampToEdge,
+    [GPU_WRAP_REPEAT] = WGPUAddressMode_Repeat,
+    [GPU_WRAP_MIRROR] = WGPUAddressMode_MirrorRepeat
+  };
+
+  static const WGPUCompareFunction compares[] = {
+    [GPU_COMPARE_NONE] = WGPUCompareFunction_Always,
+    [GPU_COMPARE_EQUAL] = WGPUCompareFunction_Equal,
+    [GPU_COMPARE_NEQUAL] = WGPUCompareFunction_NotEqual,
+    [GPU_COMPARE_LESS] = WGPUCompareFunction_Less,
+    [GPU_COMPARE_LEQUAL] = WGPUCompareFunction_LessEqual,
+    [GPU_COMPARE_GREATER] = WGPUCompareFunction_Greater,
+    [GPU_COMPARE_GEQUAL] = WGPUCompareFunction_GreaterEqual
+  };
+
+  return sampler->handle = wgpuDeviceCreateSampler(state.device, &(WGPUSamplerDescriptor) {
+    .addressModeU = wraps[info->wrap[0]],
+    .addressModeV = wraps[info->wrap[1]],
+    .addressModeW = wraps[info->wrap[2]],
+    .magFilter = filters[info->mag],
+    .minFilter = filters[info->min],
+    .mipmapFilter = filters[info->mip],
+    .lodMinClamp = info->lodClamp[0],
+    .lodMaxClamp = info->lodClamp[1],
+    .compare = compares[info->compare],
+    .maxAnisotropy = info->anisotropy
+  });
+}
+
+void gpu_sampler_destroy(gpu_sampler* sampler) {
+  //
+}
+
+// Entry
 
 bool gpu_init(gpu_config* config) {
   state.device = emscripten_webgpu_get_device();
