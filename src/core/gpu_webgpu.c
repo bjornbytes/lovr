d66b1e7ec8e754425138a616813c16661b875fe1
bjorn <mail@bjornbyt.es>
2022-12-02 23:17:32 -0800

    webgpu: device;

--- src/core/gpu_webgpu.c
+++ src/core/gpu_webgpu.c
@@ -1,10 +1,18 @@
 #include "gpu.h"
 #include <webgpu/webgpu.h>
+#include <emscripten/html5_webgpu.h>
+#include <string.h>
+
+static struct {
+  WGPUDevice device;
+} state;
 
 bool gpu_init(gpu_config* config) {
-  return false;
+  state.device = emscripten_webgpu_get_device();
+  return !!state.device;
 }
 
 void gpu_destroy(void) {
-  //
+  if (state.device) wgpuDeviceDestroy(state.device);
+  memset(&state, 0, sizeof(state));
 }
