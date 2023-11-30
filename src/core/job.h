c7a003d04fb0d61d8e460806f3fd634ce2075c35
bjorn <mail@bjornbyt.es>
2023-11-29 20:19:43 -0800

    Job system;

---
+++ src/core/job.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#pragma once
+
+typedef struct job job;
+
+typedef void fn_job(void* arg);
+
+bool job_init(uint32_t workerCount);
+void job_destroy(void);
+job* job_start(fn_job* fn, void* arg);
+void job_wait(job* job);
