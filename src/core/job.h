f6af8599848ed5af18d629ce784c33622b3f1499
bjorn <mail@bjornbyt.es>
2024-01-05 14:24:52 -0800

    job system error handling;
    - Jobs can now throw exceptions using job_abort.  This will store an
      error message on the job and immediately stop the job.  The method
      will never return.  It must be called from the worker thread running
      the job, so in practice it should only be called by the job function.
    - After waiting for the job to complete or abort using job_wait, the
      error message can be checked using job_get_error.  This is the raw
      pointer passed to job_abort, caller is responsible for freeing it or
      doing whatever necessary cleanup is required.
    - job_free must be used to recycle the job after waiting on it.
    
    Yes it uses longjmp.
    
    There's also some general cleanup, and job_start just does random jobs
    when the queue is full instead of returning NULL.

--- src/core/job.h
+++ src/core/job.h
@@ -4,10 +4,12 @@
 #pragma once
 
 typedef struct job job;
-
-typedef void fn_job(void* arg);
+typedef void fn_job(job* job, void* arg);
 
 bool job_init(uint32_t workerCount);
 void job_destroy(void);
 job* job_start(fn_job* fn, void* arg);
+void job_abort(job* job, const char* error);
 void job_wait(job* job);
+char* job_get_error(job* job);
+void job_free(job* job);
