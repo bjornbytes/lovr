7d9502babf6df9ba1be26255cd01e4caa935972d
bjorn <mail@bjornbyt.es>
2024-06-18 16:09:45 -0700

    rm lovr.getVersion test;
    This was just added to test the testing system, and will be annoying to
    keep up to date.

--- test/lovr/init.lua
+++ test/lovr/init.lua
@@ -1,8 +1,4 @@
 group('lovr', function()
-  test('getVersion', function()
-    expect({ lovr.getVersion() }).to.equal({ 0, 17, 1, 'Tritium Gourmet' })
-  end)
-
   for i, file in ipairs(lovr.filesystem.getDirectoryItems('lovr')) do
     local module = file:match('%a+')
     if lovr[module] then
