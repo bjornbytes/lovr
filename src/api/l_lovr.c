f85e6aebf7b5c210c4c549b46e9cbc8850ac5c99
bjorn <mail@bjornbyt.es>
2024-08-06 13:37:36 -0700

    Add ability to add git hash to lovr.getVersion;

--- src/api/l_lovr.c
+++ src/api/l_lovr.c
@@ -1,12 +1,19 @@
 #include "api.h"
 #include "util.h"
 
+#define STRINGIFY(x) #x
+
 static int l_lovrGetVersion(lua_State* L) {
   lua_pushinteger(L, LOVR_VERSION_MAJOR);
   lua_pushinteger(L, LOVR_VERSION_MINOR);
   lua_pushinteger(L, LOVR_VERSION_PATCH);
   lua_pushliteral(L, LOVR_VERSION_ALIAS);
+#ifdef LOVR_VERSION_HASH
+  lua_pushstring(L, STRINGIFY(LOVR_VERSION_HASH));
+  return 5;
+#else
   return 4;
+#endif
 }
 
 static const luaL_Reg lovr[] = {
