10017e335c73e8df5bd05a0e6195987eb6b68c06
bjorn <mail@bjornbyt.es>
2024-08-10 14:34:11 -0700

    test: call lots of methods on disabled colliders;

--- test/lovr/physics.lua
+++ test/lovr/physics.lua
@@ -18,8 +18,44 @@ group('physics', function()
       expect(c:isEnabled()).to.equal(false)
       c:setEnabled()
       expect(c:isEnabled()).to.equal(false)
-      c:setEnabled(true)
-      expect(c:isEnabled()).to.equal(true)
+
+      expect(c:getJoints()).to.equal({})
+      expect(c:getShapes()).to.equal({})
+
+      c:setUserData(7)
+      expect(c:getUserData()).to.equal(7)
+
+      expect(c:isKinematic()).to.equal(false)
+      c:setKinematic(true)
+      expect(c:isKinematic()).to.equal(true)
+      c:setKinematic(false)
+
+      expect(c:isSensor()).to.equal(false)
+      c:setSensor(true)
+      expect(c:isSensor()).to.equal(true)
+
+      expect(c:isContinuous()).to.equal(false)
+      c:setContinuous(true)
+      expect(c:isContinuous()).to.equal(true)
+
+      expect(c:getGravityScale()).to.equal(1.0)
+      c:setGravityScale(2.0)
+      expect(c:getGravityScale()).to.equal(2.0)
+
+      expect(c:isSleepingAllowed()).to.equal(true)
+      c:setSleepingAllowed(false)
+      expect(c:isSleepingAllowed()).to.equal(false)
+
+      expect(c:isAwake()).to.equal(false)
+      c:setAwake(true)
+      expect(c:isAwake()).to.equal(false)
+
+      c:setMass(10)
+      expect(c:getMass()).to.equal(10)
+      c:resetMassData()
+
+      c:setDegreesOfFreedom('z', 'x')
+      expect({ c:getDegreesOfFreedom() }).to.equal({ 'z', 'x' })
     end)
   end)
 
