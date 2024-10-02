b9d0d84ef0d610b1e6dca09bef0bdeb02e66223e
bjorn <mail@bjornbyt.es>
2024-10-01 19:14:27 -0700

    Also test .isFocused and .isMounted;

--- test/lovr/headset.lua
+++ test/lovr/headset.lua
@@ -2,4 +2,12 @@ group('headset', function()
   test('isVisible', function()
     expect(lovr.headset.isVisible()).to.be.a('boolean')
   end)
+
+  test('isFocused', function()
+    expect(lovr.headset.isFocused()).to.be.a('boolean')
+  end)
+
+  test('isMounted', function()
+    expect(lovr.headset.isMounted()).to.be.a('boolean')
+  end)
 end)
