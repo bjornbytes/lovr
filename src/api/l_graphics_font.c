e99e3a14fe5ea1ad3bc2cc4f78f163ce32a18757
bjorn <mail@bjornbyt.es>
2026-07-29 06:38:50 -0700

    Font:getLines wrap defaults to zero;

--- src/api/l_graphics_font.c
+++ src/api/l_graphics_font.c
@@ -139,7 +139,7 @@ static int l_lovrFontGetLines(lua_State* L) {
   uint32_t count;
   ColoredString stack;
   ColoredString* strings = luax_checkcoloredstrings(L, 2, &count, &stack);
-  float wrap = luax_checkfloat(L, 3);
+  float wrap = luax_optfloat(L, 3, 0.f);
   lua_newtable(L);
   lovrFontGetLines(font, strings, count, wrap, online, L);
   if (strings != &stack) lovrFree(strings);
